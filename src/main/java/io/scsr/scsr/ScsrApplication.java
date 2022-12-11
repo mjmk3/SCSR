c091097d036aa5bb0cb73a36496f5872c2c44da4
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-11 09:49:07 +0300

    spring react practice

---
+++ src/main/java/io/scsr/scsr/ScsrApplication.java
@@ -0,0 +1,13 @@
+package io.scsr.scsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/scsr/scsr/ScsrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.scsr.scsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
